5693a2a699cc501f3ad7dfa79d91842d3a09e72a
Inga <ijaanimets@gmail.com>
2020-09-11 17:57:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,14 @@
-def square_array(array)
+=begin def square_array(array)
     array.each do |i|
       i ** 2
-  
-   end
+  end
 end
+=end
+
+def square_array(array)
+new_numbers = []
+array.each {|num| new_nums << num ** 2}
+return new_nums
+#it numbers.each do |num|
+ #new_numbers << num ** 2
+ end
