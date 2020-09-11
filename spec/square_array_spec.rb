7ae9c39d068bd6dc5a441fbc1516f576858566a4
Inga <ijaanimets@gmail.com>
2020-09-11 17:48:50 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -1,18 +1,30 @@
-describe '#square_array' do
-
-  it 'does not call on collect/map or inject' do
+def square_array(numbers)
+new_numbers = []
+#numbers.each {|num| new_nums << num ** 2}
+#return new_nums
+it numbers.each do |num|
+ new_numbers << num ** 2
+ 
     numbers = [1,2,3]
-    expect(numbers).to_not receive(:collect)
+expect(numbers).to_not receive(:collect)
     expect(numbers).to_not receive(:map)
     expect(numbers).to_not receive(:inject)
     square_array(numbers)
-  end
+ end
+end
 
-  it 'calls on each' do
+  def square_array(numbers) 
+    numbers.each do |i|
+      i ** 2
     numbers = [1,2,3]
     expect(numbers).to receive(:each)
     square_array(numbers)
+   end
   end
+def square_array(numbers)
+array.collect do|num|
+puts num ** 2
+num ** 2
 
   it 'should square the elements in an array' do
     expect(square_array([1,2,3])).to eq([1,4,9])
